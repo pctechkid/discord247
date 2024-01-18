9c35748843d41278e28100181e197ed2484288d1
pctechkid <omboyronnel@gmail.com>
2024-01-18 22:39:30 +0800

    auth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris("MzAxNzIxODUwNDk1MzY5MjE5.G97BTw.bv8WrRNgg7BZpPIROkQM5f6O46ybTAM73EedGo");
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
